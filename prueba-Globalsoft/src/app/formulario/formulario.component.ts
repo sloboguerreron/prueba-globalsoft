b5cff7e84ae838aa600c6eb03d665088008209d4
Sebastian <novas.lobog@konradlorenz.edu.co>
2021-10-04 02:36:25 -0500

    reactive form

--- prueba-Globalsoft/src/app/formulario/formulario.component.ts
+++ prueba-Globalsoft/src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  onSave(): void{
+    if(this.contactForm.valid){
+      console.log(this.contactForm.value);
+
+    }else{
+      console.log("no es valido");
+
+    }
+  }
+  private initForm(): void{
+    this.contactForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      nombre: ['', [Validators.required]],
+      numeroCel: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/), Validators.minLength(10), Validators.maxLength(10)]],
+      mensaje: [],
+    })
+  }
 }
